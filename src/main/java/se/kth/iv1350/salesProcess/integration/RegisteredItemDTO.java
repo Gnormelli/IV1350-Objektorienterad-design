dadaf283a8d92368f19da153c605f83a384625cf
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-04-19 12:05:56 +0200

    refactoring of name and added comments

--- src/main/java/se/kth/iv1350/salesProcess/integration/RegisteredItemDTO.java
+++ src/main/java/se/kth/iv1350/salesProcess/integration/RegisteredItemDTO.java
@@ -1,17 +1,20 @@
 package se.kth.iv1350.salesProcess.integration;
 
-public class SearchItemDTO {
+/**
+ * Represent registered item identifier and quantity in the view, ready
+ * for processing in the program.
+ */
+public class RegisteredItemDTO {
     private final int quantity;
     private final int itemIdentifier;
-
 
     /**
      * Constructor for the instantiation of one item to search for.
      * 
-     * @param quantity  how many of the item for sale
+     * @param quantity  how many of the item for sale have been registered
      * @param itemIdentifier item identifier specifies a type of item for sale
      */
-    public SearchItemDTO(int quantity, int itemIdentifier) {
+    public RegisteredItemDTO(int quantity, int itemIdentifier) {
         this.quantity = quantity;
         this.itemIdentifier = itemIdentifier;
     }
