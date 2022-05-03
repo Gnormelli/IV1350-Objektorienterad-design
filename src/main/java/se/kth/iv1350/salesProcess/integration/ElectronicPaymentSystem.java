2c96aaed684a1398ddc62d3445b4af40df7ebb9b
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-03 11:09:56 +0200

    Part of design but not needed to implement.

---
+++ src/main/java/se/kth/iv1350/salesProcess/integration/ElectronicPaymentSystem.java
@@ -0,0 +1,8 @@
+package se.kth.iv1350.salesProcess.integration;
+
+/**
+ * Placeholder for Electronic payment system.
+ * Main purpose is that no change will have to be returned
+ */
+public class ElectronicPaymentSystem {
+}
