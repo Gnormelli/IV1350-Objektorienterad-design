e08163d39e24284555cbdba53380955a8a409197
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-03 11:48:37 +0200

    Implemented and ready to use. More tests needed but the existing pass

--- src/main/java/se/kth/iv1350/salesProcess/view/View.java
+++ src/main/java/se/kth/iv1350/salesProcess/view/View.java
@@ -1,15 +1,20 @@
 package se.kth.iv1350.salesProcess.view;
 
 import se.kth.iv1350.salesProcess.controller.Controller;
-import se.kth.iv1350.salesProcess.integration.ItemDTO;
-import se.kth.iv1350.salesProcess.integration.RegisteredItemDTO;
+import se.kth.iv1350.salesProcess.integration.*;
+import se.kth.iv1350.salesProcess.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * A placeholder for the real view with hardcoded calls to simulate the
  * system operations to the controller.
  */
 public class View {
-    private Controller contr;
+    private final Controller contr;
+    private List<RegisteredItemDTO> basketOfItems;
 
     /**
      * Constructor for the view.
@@ -23,11 +28,84 @@ public class View {
     /**
      * Placeholder that simulates user input to this program.
      */
-    public void simulatorOfExecution(){
+    public void simulatorOfExecution(Scanner scan){
+        SalesItem addedItem;
+
         contr.startNewSale();
         System.out.println("A new sale has been started.");
 
-        RegisteredItemDTO itemBeingRegistered = new RegisteredItemDTO(3,55555);
-        ItemDTO foundItem = contr.searchForItemIdentifier(itemBeingRegistered);
+        basketOfItems = new ArrayList<>();
+        addBasketOfItems();
+        for(RegisteredItemDTO item : basketOfItems){
+             addedItem = contr.addItemToSale(item);
+            System.out.println(printUpdateInList(addedItem));
+        }
+        float totalPriceIncVAT = contr.endSale();
+        System.out.println("Total price inc VAT: " + totalPriceIncVAT + " kr");
+
+        boolean wantsDiscount = askIfCustomerWantsDiscount(scan);
+        if(wantsDiscount){
+            float amountToPay = contr.requestDiscount(1);
+            System.out.println("Total amount to pay incl discount: " + amountToPay + " kr");
+        }
+
+        float paidAmount = 400;
+        System.out.println("Paid amount: " + paidAmount + " kr");
+        contr.enterCashPayment(paidAmount);
+
+    }
+
+
+
+    /**
+     * Print the update in the list of items
+     * @param addedItem the item that has been added
+     * @return string of update in list.
+     */
+    private String printUpdateInList(SalesItem addedItem){
+        int quantity = addedItem.getQuantityOfItem();
+        String itemName = addedItem.getSalesItem().getName();
+        float priceExclVAT = addedItem.totalPriceExclVATForSalesItem();
+        float VAT = addedItem.totalVATForSalesItem();
+        float priceIncVAt = addedItem.totalPriceIncVATForSalesItem();
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Item name: ").append(itemName).append(", ");
+        builder.append("Quantity: " + quantity + ", ");
+        builder.append("Prince Excl VAT : " + priceExclVAT + "kr, ");
+        builder.append("VAT: " + VAT + "kr, ");
+        builder.append("Total price: " + priceIncVAt + "kr");
+
+        return builder.toString();
+    }
+
+    /**
+     * Cashier asks customr if they want discount
+     * @param scan scanner for the program
+     * @return boolean igf they want discount
+     */
+    private boolean askIfCustomerWantsDiscount(Scanner scan){
+        System.out.println("Customer whats discount? Enter YES or NO: \n");
+        String wantDiscountString = scan.nextLine();
+        boolean wantDiscount;
+
+        if (wantDiscountString.equals("YES")){
+            wantDiscount = true;
+        }else{
+            wantDiscount = false;
+        }
+        return wantDiscount;
+    }
+
+    /**
+     * Placeholder for the basket of items to be added to the sale
+     */
+    private void addBasketOfItems(){
+        basketOfItems.add(new RegisteredItemDTO(3,55555));
+        basketOfItems.add(new RegisteredItemDTO(2,333));
+        basketOfItems.add(new RegisteredItemDTO(3,55555));
+        basketOfItems.add(new RegisteredItemDTO(1,333));
+        basketOfItems.add(new RegisteredItemDTO(2,1));
+        basketOfItems.add(new RegisteredItemDTO(1,55555));
     }
 }
--- src/test/java/se/kth/iv1350/salesProcess/view/ViewTest.java
+++ src/test/java/se/kth/iv1350/salesProcess/view/ViewTest.java
@@ -4,35 +4,48 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import se.kth.iv1350.salesProcess.controller.Controller;
+import se.kth.iv1350.salesProcess.integration.CashRegister;
 import se.kth.iv1350.salesProcess.integration.ExternalSystemStartUp;
 import se.kth.iv1350.salesProcess.integration.Printer;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ViewTest {
+    private View instanceToTest;
+
     private ExternalSystemStartUp creator;
     private Printer printer;
-    private View instanceToTest;
+    private CashRegister cashRegister;
+    private Controller contr;
+    private Scanner scan;
 
     private ByteArrayOutputStream printoutBuffer;
     private PrintStream originalStream;
 
     @BeforeEach
     void setUp() {
-    Controller contr = new Controller(creator, printer);
-    instanceToTest = new View(contr);
+        creator = new ExternalSystemStartUp();
+        printer = new Printer();
+        cashRegister = new CashRegister();
+        contr = new Controller(creator, printer, cashRegister);
+        instanceToTest = new View(contr);
 
-    printoutBuffer = new ByteArrayOutputStream();
-    PrintStream inMemSysOut = new PrintStream(printoutBuffer);
-    originalStream = System.out;
-    System.setOut(inMemSysOut);
+        printoutBuffer = new ByteArrayOutputStream();
+        PrintStream inMemSysOut = new PrintStream(printoutBuffer);
+        originalStream = System.out;
+        System.setOut(inMemSysOut);
     }
 
     @AfterEach
     void tearDown() {
+        creator = null;
+        printer = null;
+        cashRegister = null;
+        contr = null;
         instanceToTest = null;
 
         printoutBuffer = null;
@@ -44,7 +57,7 @@ class ViewTest {
      */
     @Test
     void testSimulatorOfExecutionNewSalePrintout() {
-        instanceToTest.simulatorOfExecution();
+        instanceToTest.simulatorOfExecution(scan);
         String printout = printoutBuffer.toString();
         String expectedOutput = "new sale";
         assertTrue(printout.contains(expectedOutput),"UI did not start correctly");
