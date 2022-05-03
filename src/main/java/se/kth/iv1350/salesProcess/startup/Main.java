ffba9a9433fb52eb2c61964004db829a55794c4d
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-03 11:23:46 +0200

    Added cashRegister, scanner and extra simulator run to show difference in making choice of discount or not.

--- src/main/java/se/kth/iv1350/salesProcess/startup/Main.java
+++ src/main/java/se/kth/iv1350/salesProcess/startup/Main.java
@@ -3,7 +3,10 @@ package se.kth.iv1350.salesProcess.startup;
 import se.kth.iv1350.salesProcess.controller.Controller;
 import se.kth.iv1350.salesProcess.integration.ExternalSystemStartUp;
 import se.kth.iv1350.salesProcess.integration.Printer;
+import se.kth.iv1350.salesProcess.integration.CashRegister;
 import se.kth.iv1350.salesProcess.view.View;
+
+import java.util.Scanner;
 
 public class Main {
     /**
@@ -12,10 +15,14 @@ public class Main {
      */
     public static void main(String[] args) {
         ExternalSystemStartUp creator = new ExternalSystemStartUp();
+
+        CashRegister cashRegister = new CashRegister();
         Printer printer = new Printer();
-        Controller contr = new Controller(creator, printer);
+        Controller contr = new Controller(creator, printer, cashRegister);
 
         View view = new View(contr);
-        view.simulatorOfExecution();
+        Scanner scan = new Scanner(System.in);
+        view.simulatorOfExecution(scan);
+        view.simulatorOfExecution(scan);
     }
 }
