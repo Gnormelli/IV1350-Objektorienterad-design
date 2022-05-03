e1a47baa1e0d10b1f41b3c8ed5920d93efd949b4
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-03 11:08:59 +0200

    Not yet written

---
+++ src/test/java/se/kth/iv1350/salesProcess/model/DiscountTest.java
@@ -0,0 +1,38 @@
+package se.kth.iv1350.salesProcess.model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAmountQuantityDiscount() {
+    }
+
+    @Test
+    void getAmountPercentageDiscount() {
+    }
+
+    @Test
+    void getQuantityDiscount() {
+    }
+
+    @Test
+    void getDiscountItem() {
+    }
+
+    @Test
+    void getRuleForDiscount() {
+    }
+}
