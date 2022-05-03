e74576f7c2b748a19842d430b8aaa3bf6b9e5357
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-03 11:25:04 +0200

    Could use more test. Not complete but passes the test.

--- src/test/java/se/kth/iv1350/salesProcess/startup/MainTest.java
+++ src/test/java/se/kth/iv1350/salesProcess/startup/MainTest.java
@@ -4,21 +4,40 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+    private Main instanceToTest;
+    private ByteArrayOutputStream printoutBuffer;
+    private PrintStream originalStream;
 
     @BeforeEach
     void setUp() {
+        instanceToTest = new Main();
+
+        printoutBuffer = new ByteArrayOutputStream();
+        PrintStream inMemSysOut = new PrintStream(printoutBuffer);
+        originalStream = System.out;
+        System.setOut(inMemSysOut);
     }
 
     @AfterEach
     void tearDown() {
+        instanceToTest = null;
+
+        printoutBuffer = null;
+        System.setOut(originalStream);
     }
 
     @Test
-    public void main() {
+    public void testUIHasStarted() {
         String[] args = null;
         Main.main(args);
+        String printout = printoutBuffer.toString();
+        String expectedOutput = "new sale";
+        assertTrue(printout.contains(expectedOutput),"UI did not start correctly");
     }
 }
