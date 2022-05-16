399cfd61976e0360c3d7468c8194aaef8136aa7f
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2022-05-16 16:47:50 +0200

    Refactored observer interface to take float

--- src/main/java/se/kth/iv1350/salesProcess/integration/Observer.java
+++ src/main/java/se/kth/iv1350/salesProcess/integration/Observer.java
@@ -1,4 +1,4 @@
-package se.kth.iv1350.salesProcess.model;
+package se.kth.iv1350.salesProcess.integration;
 
 /**
  * Observing interface for classes to get notified of settlement of sale with payment.
@@ -8,7 +8,7 @@ public interface Observer {
     /**
      * Invoked when sale is paid for and receipt is to be printed.
      *
-     * @param salesInfo Sale that is settled.
+     * @param income from Sale that is settled.
      */
-    void settledSale(Sale salesInfo );
+    void settledSale(float income );
 }
